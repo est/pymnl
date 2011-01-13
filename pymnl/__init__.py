6ba8e471388f9f3c2587ee164bd1224ea9746a65
Sean Robinson <seankrobinson@gmail.com>
2011-01-13 05:13:13 -0700

    Add skeleton of module initializer
    Signed-off-by: Sean Robinson <seankrobinson@gmail.com>

---
+++ pymnl/__init__.py
@@ -0,0 +1,24 @@
+# pymnl -- a minimalistic pure Python interface for netlink
+# Copyright 2011 Sean Robinson <seankrobinson@gmail.com>
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public License
+#  as published by the Free Software Foundation; either version 2.1 of
+#  the License, or (at your option) any later version.
+#
+#  This library is distributed in the hope that it will be useful, but
+#  WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+#  USA
+
+# This module is heavily influenced by the excellent libmnl
+# from Pablo Neira Ayuso <pablo@netfilter.org>.  However,
+# pymnl does not use libmnl.
+
+from ctypes import *
+
