579ecb1d979efac5f2f2e3767939b97317cfc331
Sean Robinson <seankrobinson@gmail.com>
2011-01-15 20:16:35 -0700

    Add Attr init
    Signed-off-by: Sean Robinson <seankrobinson@gmail.com>

--- pymnl/attributes.py
+++ pymnl/attributes.py
@@ -19,4 +19,36 @@
 #  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
 #  USA
 #
+# Much of the method docstrings are from libmnl and are
+#      Copyright 2008-2010 by Pablo Neira Ayuso <pablo@netfilter.org>
+#
+
+from struct import calcsize, pack, unpack
+
+class Attr:
+    # pack/unpack format for type and length
+    header_format = "hh"
+
+    def __init__(self, type=None, value=None, packed_data=None):
+        """ Netlink Type-Length-Value (TLV) attribute:
+
+            |<-- 2 bytes -->|<-- 2 bytes -->|<-- variable -->|
+            -------------------------------------------------
+            |     length    |      type     |      value     |
+            -------------------------------------------------
+            |<--------- header ------------>|<-- payload --->|
+
+            The payload of the Netlink message contains sequences of
+            attributes that are expressed in TLV format.
+        """
+        self._type = type
+        self._value = value
+        if (packed_data):
+            (self._length,
+             self._type) = unpack(Attr.header_format, packed_data[:4])
+            self._value = packed_data[4:]
+        if (self._value):
+            self._value_format = repr(len(self._value)) + "s"
+
+
 
