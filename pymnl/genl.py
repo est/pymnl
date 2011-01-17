10392c38738965768b3e90db7e47260146c6787f
Sean Robinson <seankrobinson@gmail.com>
2011-01-17 08:01:30 -0700

    Define constants for generic netlink
    Adapted from linux/genetlink.h.
    
    Signed-off-by: Sean Robinson <seankrobinson@gmail.com>

--- pymnl/genl.py
+++ pymnl/genl.py
@@ -20,3 +20,22 @@
 #  USA
 #
 
+import pymnl
+
+#
+# linux/genetlink.h
+#
+GENL_NAMSIZ = 16     # length of family name
+
+GENL_MIN_ID = pymnl.NLMSG_MIN_TYPE
+GENL_MAX_ID = 1023
+
+GENL_ADMIN_PERM = 0x01
+GENL_CMD_CAP_DO = 0x02
+GENL_CMD_CAP_DUMP = 0x04
+GENL_CMD_CAP_HASPOL = 0x08
+
+# List of reserved static generic netlink identifiers:
+GENL_ID_GENERATE = 0
+GENL_ID_CTRL = pymnl.NLMSG_MIN_TYPE
+
