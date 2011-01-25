9b57be794d2efaed90d7371f2779f393c8955917
Sean Robinson <seankrobinson@gmail.com>
2011-01-25 04:33:38 -0700

    Add ctrl_attr_maxattr() to save genl family's max attribute
    Signed-off-by: Sean Robinson <seankrobinson@gmail.com>

--- pymnl/genl.py
+++ pymnl/genl.py
@@ -87,6 +87,7 @@ class GenlAttrParser(AttrParser):
                     CTRL_ATTR_FAMILY_NAME : self.ctrl_attr_family_name,
                     CTRL_ATTR_VERSION : self.ctrl_attr_version,
                     CTRL_ATTR_HDRSIZE : self.ctrl_attr_hdrsize,
+                    CTRL_ATTR_MAXATTR : self.ctrl_attr_maxattr,
                     CTRL_ATTR_OPS : self.ctrl_attr_ops,
                     CTRL_ATTR_MCAST_GROUPS : self.ctrl_attr_mcast_groups}
 
@@ -117,6 +118,13 @@ class GenlAttrParser(AttrParser):
             attr - Attr object
         """
         self._attributes['hdrsize'] = attr.get_u32()
+
+    def ctrl_attr_maxattr(self, attr):
+        """ Save maximum attribute number.
+
+            attr - Attr object
+        """
+        self._attributes['maxattr'] = attr.get_u32()
 
     def ctrl_attr_ops(self, attr):
         """ Print attribute type and parse nested attributes.
