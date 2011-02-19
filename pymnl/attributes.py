d40dff9cf19b4186658d1767511c4599e5dfd530
Sean Robinson <seankrobinson@gmail.com>
2011-02-19 03:28:00 -0700

    Explicitly state null is a byte string value
    Small change towards Python3 compatibility.
    
    Signed-off-by: Sean Robinson <seankrobinson@gmail.com>

--- pymnl/attributes.py
+++ pymnl/attributes.py
@@ -268,7 +268,7 @@ class Attr:
         # prepare the header info
         header = pack(header_format, len(self), self._type)
         # prepare the null padding
-        pad = (NLA_ALIGN(len(self._value)) - len(self._value)) * "\x00"
+        pad = (NLA_ALIGN(len(self._value)) - len(self._value)) * b'\x00'
         # push the whole package out
         return header + self._value + pad
 
