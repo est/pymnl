3efc76baca0525a266b5bb310fcac169f2146d3d
Sean Robinson <seankrobinson@gmail.com>
2011-02-19 17:21:17 -0700

    Add prototypical integer length private test method
    Signed-off-by: Sean Robinson <seankrobinson@gmail.com>

--- tests/attributes.py
+++ tests/attributes.py
@@ -47,6 +47,28 @@ class TestAttributes(unittest.TestCase):
             for value_ in (-1, random_, -1 * max_value_, "test string"):
                 self.assertRaises(TypeError, method_, type_, value_)
 
+    def _test_integer_length(self, method_, max_value_, aligned_len_):
+        """ Test the specified integer length.
+
+            method_ - Attr constructor to test
+
+            max_value_ - maximum value the integer can hold
+
+            aligned_len_ - length of Attr value aligned to NLA size (in
+                bytes); this value should not include the attribute header
+        """
+        for type_ in (TYPE_U8, TYPE_U16, TYPE_U32, TYPE_U64):
+            random_ = randint(1, max_value_ - 1)
+            for value_ in (0, random_, max_value_):
+                one_attr = method_(type_, value_)
+                # valid values
+                self.assertEqual(len(one_attr), ATTR_HDRLEN + aligned_len_,
+                            "length does not match expected Attr length")
+                # invalid values
+                self.assertNotEqual(len(method_(type_, value_)),
+                            ATTR_HDRLEN + randint(0, aligned_len_ - 1),
+                            "length unexpectedly matches")
+
     def test_new_u8(self):
         """ Test new_u8() constructor.
         """
