e1df32de415eb8273c0e761de8761fcd846f9182
Sean Robinson <seankrobinson@gmail.com>
2011-03-07 17:08:34 -0700

    Add if_ module to represent constants, etc. from net/if.h
    Just add the minimal info to support rtnl.
    
    Signed-off-by: Sean Robinson <seankrobinson@gmail.com>

---
+++ examples/rtnl/if_.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+#
+# if_.py -- constants and data structures from net/if.h
+# Copyright 2011 Sean Robinson <seankrobinson@gmail.com>
+#
+# This file is part of the pymnl package, a Python interface
+# for netlink sockets.
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public License
+#  as published by the Free Software Foundation; either version 2.1 of
+#  the License, or (at your option) any later version.
+#
+#  This library is distributed in the hope that it will be useful, but
+#  WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+#  USA
+#
+
+from struct import calcsize, pack, unpack
+
+IFF_RUNNING = 0x40
+
+class IfInfoMessage(object):
+    """ Passes link level specific information, not dependent on network
+        protocol. -- /usr/include/linux/rtnetlink.h
+    """
+    def __init__(self, contents=None):
+        """
+        """
+        self._format = "BBHiII"
+        self.family = 0
+        self.pad = 0
+        self.type_ = 0
+        self.index = 0
+        self.flags = 0
+        self.change = 0
+        if (contents):
+            contents = contents[:calcsize(self._format)]
+            (self.family,
+             self.pad,
+             self.type_,
+             self.index,
+             self.flags,
+             self.change) = unpack(self._format, contents)
+
+    def __len__(self):
+        """
+        """
+        return calcsize(self._format)
+
