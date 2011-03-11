a778c5fc725a287d167c82b5dad36c41bd075365
Sean Robinson <seankrobinson@gmail.com>
2011-03-10 17:07:01 -0700

    Add rtmsg constants
    Signed-off-by: Sean Robinson <seankrobinson@gmail.com>

--- examples/rtnl/rtnetlink.py
+++ examples/rtnl/rtnetlink.py
@@ -66,3 +66,64 @@ class RtGenMessageHeader(object):
         return pack(self._format, self._family)
 
 
+# rtm_type
+RTN_UNSPEC = 0
+RTN_UNICAST = 1
+RTN_LOCAL = 2
+RTN_BROADCAST = 3
+RTN_ANYCAST = 4
+RTN_MULTICAST = 5
+RTN_BLACKHOLE = 6
+RTN_UNREACHABLE = 7
+RTN_PROHIBIT = 8
+RTN_THROW = 9
+RTN_NAT = 10
+RTN_XRESOLVE = 11
+__RTN_MAX = 12
+
+# rtm_protocol
+RTPROT_UNSPEC = 0
+RTPROT_REDIRECT = 1
+RTPROT_KERNEL = 2   # route installed by kernel
+RTPROT_BOOT = 3     # route installed during boot
+RTPROT_STATIC = 4   # route installed by administrator
+# Values >= RTPROT_STATIC are not interpreted by kernel, they are
+# just user-defined.
+
+# rtm_scope
+#
+# Really it is not scope, but sort of distance to the destination.
+# NOWHERE are reserved for not existing destinations, HOST is our
+# local addresses, LINK are destinations, located on directly attached
+# link and UNIVERSE is everywhere in the Universe.
+#
+# Intermediate values are also possible f.e. interior routes
+# could be assigned a value between UNIVERSE and LINK.
+RT_SCOPE_UNIVERSE = 0     # everywhere in the universe
+#    ... user defined values ...
+RT_SCOPE_SITE = 200
+RT_SCOPE_LINK = 253       # destination attached to link
+RT_SCOPE_HOST = 254       # local address
+RT_SCOPE_NOWHERE  = 255   # not existing destination
+
+# rtm_flags
+RTM_F_NOTIFY = 0x100    # notify user of route change
+RTM_F_CLONED = 0x200    # this route is cloned
+RTM_F_EQUALIZE = 0x400  # Multipath equalizer: NI
+RTM_F_PREFIX = 0x800    # Prefix addresses
+
+# Reserved table identifiers
+RT_TABLE_UNSPEC = 0
+# ... user defined values ...
+RT_TABLE_COMPAT = 252
+RT_TABLE_DEFAULT = 253
+RT_TABLE_MAIN = 254
+RT_TABLE_LOCAL = 255
+RT_TABLE_MAX = 0xFFFFFFFF
+# Synonimous attribute: RTA_TABLE.
+
+# Routing message attributes
+RTA_DST = 1
+RTA_OIF = 4
+RTA_GATEWAY = 5
+
