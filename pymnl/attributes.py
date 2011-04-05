4521eb09057382fa1046aa3d294b66cf097bec50
Sean Robinson <seankrobinson@gmail.com>
2011-04-05 03:12:20 -0700

    Check string length for all retrieved strings
    Consolidate string length test to one location that all code paths will
    hit.
    
    Signed-off-by: Sean Robinson <seankrobinson@gmail.com>

--- pymnl/attributes.py
+++ pymnl/attributes.py
@@ -251,9 +251,9 @@ class Attr(object):
             should have a non-zero length and a null termination
             or something went wrong.
         """
+        if (self.get_value_len() == 0):
+            raise TypeError("String attribute is too short")
         if (type == TYPE_NUL_STRING):
-            if (self.get_value_len() == 0):
-                raise TypeError("String attribute is too short")
             if (self._value[-1:] != 0):
                 raise TypeError("This attribute is not null-terminated," +
                                 "as it claims to be")
@@ -261,12 +261,7 @@ class Attr(object):
 
     def get_str_stripped(self):
         """ Return value as a string, without zero terminator.
-
-            Raises TypeError if the data length is zero.
-            A string should have a non-zero length or something went wrong.
         """
-        if (type == TYPE_STRING and self.get_value_len() == 0):
-            raise TypeError("String attribute is too short")
         string_ = self.get_str()
         if ((string_[-1] == b'\x00') or (string_[-1] == 0)):
             # b'\x00' works in Py2, but 0 works in Py3, gah!
